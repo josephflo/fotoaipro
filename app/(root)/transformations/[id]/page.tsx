4fcc492e72031b47e55b2832449eec6b36e89862
Joseph Flores <105085111+josephflo@users.noreply.github.com>
2024-03-09 16:43:23 -0300

    layout

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransformationsPage = ({ params: { id } }) => {
+  return (
+    <div>TransformationsPage {id}</div>
+  )
+}
+
+export default TransformationsPage
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const IBMPlex = IBM_Plex_Sans({ subsets: ["latin"],
-  weight:['400', '500', '600', '700'],
-  variable: '--font-ibm-plex'
+import "./globals.css";
+
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex",
 });
 
 export const metadata: Metadata = {
@@ -19,9 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>{children}
-      </body>
-    </html>
+    <ClerkProvider appearance={{
+      variables: {colorPrimary:'#624cf5'}
+    }}>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
