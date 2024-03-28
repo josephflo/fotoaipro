3591ee894725a56d2676f4ff998a4a278fa34d46
Joseph Flores <105085111+josephflo@users.noreply.github.com>
2024-03-28 05:33:56 -0300

    feat/banner

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,7 +70,7 @@ module.exports = {
       },
       backgroundImage: {
         "purple-gradient": "url('/assets/images/gradient-bg.svg')",
-        banner: "url('/assets/images/banner-bg.png')",
+        banner: "url('/assets/images/banner-bg-2.png')",
       },
       borderRadius: {
         lg: "var(--radius)",
